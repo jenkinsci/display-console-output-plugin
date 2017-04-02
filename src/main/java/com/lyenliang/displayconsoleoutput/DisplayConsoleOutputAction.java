b18e4f10e0bc89bbb28cde321d7b49d700189d90
lyenliang <lyenliang@gmail.com>
2017-04-02 15:47:08 +0800

    Initial commit

---
+++ src/main/java/com/lyenliang/displayconsoleoutput/DisplayConsoleOutputAction.java
@@ -0,0 +1,25 @@
+package com.lyenliang.displayconsoleoutput;
+
+import hudson.model.Action;
+
+public class DisplayConsoleOutputAction implements Action {
+
+	@Override
+	public String getIconFileName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUrlName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
