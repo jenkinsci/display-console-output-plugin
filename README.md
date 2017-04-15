257fd0e4dd7c9aabffbda61a76f63a1d268f6c39
lyenliang <lyenliang@gmail.com>
2017-04-15 16:30:51 +0800

    Refactor and adjust layout.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ This plugin adds console output of last build to the project page.
 
 You can also view the console output of an in progress build with this plugin.
 
+[Wiki page](https://wiki.jenkins-ci.org/display/JENKINS/Display+Console+Output+Plugin) for this plugin.
 
 ![Display Console Output Plugin](img/display-console-output-plugin.png)
--- src/main/java/jenkins/plugins/displayconsoleoutput/DisplayConsoleOutputAction.java
+++ src/main/java/jenkins/plugins/displayconsoleoutput/DisplayConsoleOutputAction.java
@@ -8,16 +8,17 @@ import edu.umd.cs.findbugs.annotations.CheckForNull;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
-import hudson.model.Run;
 
-public class DisplayConsoleOutputAction<P extends AbstractProject<P, R>, R extends AbstractBuild<P, R>> implements Action {
+public class DisplayConsoleOutputAction<P extends AbstractProject<P, R>, R extends AbstractBuild<P, R>>
+		implements Action {
 
 	private final P project;
-	
+	private long threshold = 4096L;
+
 	public DisplayConsoleOutputAction(P project) {
 		this.project = project;
 	}
-	
+
 	@Override
 	public String getIconFileName() {
 		return null;
@@ -32,39 +33,35 @@ public class DisplayConsoleOutputAction<P extends AbstractProject<P, R>, R exten
 	public String getUrlName() {
 		return null;
 	}
-	
+
 	public int getLastBuildNumber() {
-    	if (project == null) {
-            return 0;
-        }
-    	return project.getLastBuild().getNumber();
-    }
-	
+		if (project == null) {
+			return 0;
+		}
+		return project.getLastBuild().getNumber();
+	}
+
 	@CheckForNull
-    public R getBuild() {
-        if (project == null) {
-            return null;
-        }
-        R build = project.getLastBuild();
-        return build;
-    }
+	public R getBuild() {
+		if (project == null) {
+			return null;
+		}
+		return project.getLastBuild();
+	}
 
-    public boolean isComplete() throws IOException {
-        R build = getBuild();
-        return build == null || build.getLogText().length() < 4096L;
-        
-    }
-    
-    public Run getRun() {
-    	R build = getBuild();
-    	return build;
-    }
+	public boolean isTooLong() {
+		R build = getBuild();
+		if (build == null) {
+			return false;
+		}
+		return build.getLogText().length() >= threshold;
+	}
 
-    public void writeLogTo(XMLOutput out) throws IOException {
-        R build = getBuild();
-        if (build != null) {
-            build.getLogText().writeHtmlTo(Math.max(0L, build.getLogText().length() - 4096L), out.asWriter());
-        }
-    }
+	public void writeLogTo(XMLOutput out) throws IOException {
+		R build = getBuild();
+		if (build != null) {
+			build.getLogText().writeHtmlTo(Math.max(0L, build.getLogText().length() - threshold), out.asWriter());
+		}
+	}
 
 }
